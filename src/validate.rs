7be57d076803bc07038e4ab76a1020b8c26392af
JoonHo Son <joonho.son@me.com>
2025-01-24 11:50:30 +0900

    refactor: string::validate_email test 이관

--- src/validate.rs
+++ src/validate.rs
@@ -2,7 +2,10 @@
 
 use regex::bytes::Regex;
 
-const EMAIL_EXPRESSION: &str = r"^[\w-]+(\.[\w-]+)*@([A-Za-z0-9-]+\.)+[A-Za-z]{2,4}$";
+/// 다음 항목 참고
+/// - https://stackoverflow.com/a/201378
+/// - https://stackoverflow.com/a/2049510
+const EMAIL_EXPRESSION: &str = r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#;
 
 /// 이메일 유효성 검사
 ///
@@ -33,7 +36,7 @@ const EMAIL_EXPRESSION: &str = r"^[\w-]+(\.[\w-]+)*@([A-Za-z0-9-]+\.)+[A-Za-z]{2
 ///
 /// let email = "email!#$@email.com";
 ///
-/// assert!(!validate_email(email));
+/// assert!(validate_email(email));
 ///
 /// let email = "@email.com";
 ///
@@ -58,8 +61,51 @@ mod tests {
         assert!(validate_email("email@email.com"));
         assert!(!validate_email("email"));
         assert!(!validate_email("email@"));
-        assert!(!validate_email("email!#$@email.com"));
+        assert!(validate_email("email!#($@email.com"));
         assert!(!validate_email("@email.com"));
         assert!(!validate_email("@email@"));
+    }
+
+    #[test]
+    fn validate_email_test() {
+        let mut email = "joonho.son@me.com";
+
+        assert!(validate_email(email), "정상적인 이메일 유효성 검사 실패");
+
+        email = "test@test";
+
+        assert!(!validate_email(email));
+
+        email = "test@test.";
+
+        assert!(!validate_email(email));
+
+        email = "";
+
+        assert!(!validate_email(email));
+    }
+
+    #[test]
+    fn korean_domain_fail_test() {
+        let mut email = "한글ID@test.com";
+
+        assert!(
+            !validate_email(email),
+            "한글 ID를 포함하는 이메일 검사 실패"
+        );
+
+        email = "test@한글도메인.com";
+
+        assert!(
+            !validate_email(email),
+            "한글 도메인을 포함하는 이메일 검사 실패"
+        );
+
+        email = "홍길동@한글도메인.com";
+
+        assert!(
+            !validate_email(email),
+            "한글 ID 및 한글 도메인을 포함하는 이메일 검사 실패"
+        );
     }
 }
